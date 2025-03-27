bccc882b29b777dcd1742698ace1b24823d75687
Cohen <1768065921@qq.com>
2025-03-27 17:50:15 +0800

    add files

---
+++ DedsiCaCodeGeneration/Program.cs
@@ -0,0 +1,9 @@
+﻿// https://github.com/scriban/scriban
+
+using Scriban;
+
+var text = await File.ReadAllTextAsync("./TemplateFiles/Domain");
+
+var template = Template.Parse(text);
+var result = template.Render(new { projectname = "World", domainname = "Dedsi" });
+Console.WriteLine(result);
