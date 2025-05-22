aace9154a4791801c48d7b094d02c8af472fa1ce
Cohen <1768065921@qq.com>
2025-05-22 14:33:18 +0800

    添加跨域
    添加跨域

--- DedsiCaCodeGenerationApi/Program.cs
+++ DedsiCaCodeGenerationApi/Program.cs
@@ -5,6 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+// ¿ª·Å¿çÓòÇëÇó
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
