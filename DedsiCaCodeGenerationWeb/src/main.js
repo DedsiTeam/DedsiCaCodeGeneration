d329dde5c08e23e76847c80701e267dea6815b39
Cohen <1768065921@qq.com>
2025-05-23 11:18:37 +0800

    add vue-router
    add vue-router

--- DedsiCaCodeGenerationWeb/src/main.js
+++ DedsiCaCodeGenerationWeb/src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue';
 import arcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
 
+import router from './router'
+
 const app = createApp(App);
-app.use(arcoVue);
+app.use(router).use(arcoVue);
 app.mount('#app');
